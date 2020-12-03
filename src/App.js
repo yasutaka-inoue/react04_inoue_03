a86beed80f65663dc01228d4df8c8f990b08f61c
yasutaka-inoue <69379382+yasutaka-inoue@users.noreply.github.com>
2020-12-03 18:05:23 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,33 @@ import React from "react";
 import Nav from "./Nav";
 import Feed from "./Feed";
 import InputTogo from "./InputTogo";
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#35baf6',
+      main: '#03a9f4',
+      dark: '#0276aa',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ffcf33',
+      main: '#ffc400',
+      dark: '#b28900',
+      contrastText: '#000',
+    },
+  },
+});
 
 const App = () => {
   return (
     <>
+      <ThemeProvider theme={theme}>
       <Nav/>
       <InputTogo/>
       <Feed/>
+      </ThemeProvider>
     </>
   );
 };
