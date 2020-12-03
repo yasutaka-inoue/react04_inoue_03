92ccb85972f7b78fa7a70aa20149b988958638e8
yasutaka-inoue <69379382+yasutaka-inoue@users.noreply.github.com>
2020-12-03 18:06:00 +0900

    Update Nav.js

--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,8 @@
 import { makeStyles} from '@material-ui/core/styles';
-import { AppBar, IconButton, Toolbar, Typography } from '@material-ui/core'
+import { AppBar, IconButton, ThemeProvider, Toolbar, Typography } from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu';
 import React from 'react'
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -19,9 +20,9 @@ const Nav = () => {
     const classes = useStyles();
     return (
         <div className={classes.root}>
-          <AppBar position="static"z>
+          <AppBar position="static">
             <Toolbar>
-              <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <IconButton edge="start" className={classes.menuButton} color="primary" aria-label="menu">
                 <MenuIcon />
               </IconButton>
               <Typography variant="h6" className={classes.title}>
